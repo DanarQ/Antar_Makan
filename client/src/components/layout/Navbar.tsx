05baa3b940311f16dae0638e6a6c3776f08a1710
Danar Qusyairi <danar4213@gmail.com>
2026-04-07 12:09:22 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   client/.gitignore
    	new file:   client/README.md
    	new file:   client/eslint.config.js
    	new file:   client/index.html
    	new file:   client/package-lock.json
    	new file:   client/package.json
    	new file:   client/public/favicon.svg
    	new file:   client/public/icons.svg
    	new file:   client/src/App.css
    	new file:   client/src/App.tsx
    	new file:   client/src/assets/hero.png
    	new file:   client/src/assets/react.svg
    	new file:   client/src/assets/vite.svg
    	new file:   client/src/components/layout/Navbar.css
    	new file:   client/src/components/layout/Navbar.tsx
    	new file:   client/src/index.css
    	new file:   client/src/main.tsx
    	new file:   client/tsconfig.app.json
    	new file:   client/tsconfig.json
    	new file:   client/tsconfig.node.json
    	new file:   client/vite.config.ts

---
+++ client/src/components/layout/Navbar.tsx
@@ -0,0 +1,21 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>ANTAR MAKAN</h1>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
